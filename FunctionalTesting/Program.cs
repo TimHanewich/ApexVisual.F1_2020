eae894cc5c1981edcf20e04604de345ef6f8ef90
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-07-22 22:19:08 -0700

    Initial commit

---
+++ FunctionalTesting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ApexVisual.F1_2020;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace FunctionalTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ApexVisualManager avm = ApexVisualManager.Create("<Connection string here>");
+            
+            string path = "C:\\Users\\TaHan\\Downloads\\Telemetry 7-14-2020 cacc16ea-4bb2-47d6-a386-ead3bf20c54f.json";
+
+            string content = System.IO.File.ReadAllText(path);
+
+            List<byte[]> data = JsonConvert.DeserializeObject<List<byte[]>>(content);
+
+            Console.WriteLine("Uploading now...");
+            avm.UploadSessionAsync(data).Wait();
+
+            Console.WriteLine("Done!");
+
+        }
+    }
+}
---
+++ src/ApexVisualManager.cs
@@ -0,0 +1,119 @@
+using System;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.Storage;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Codemasters.F1_2020;
+using Codemasters.F1_2020.Analysis;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ApexVisual.F1_2020
+{
+    public class ApexVisualManager
+    {
+        private string con_str = "";
+        private CloudStorageAccount csa;
+        private CloudBlobClient cbc;
+
+        public static ApexVisualManager Create(string connection_string)
+        {
+            ApexVisualManager ToReturn = new ApexVisualManager();
+
+            //Save con string
+            ToReturn.con_str = connection_string;
+
+            //Make the connection
+            bool succeededconnection = CloudStorageAccount.TryParse(connection_string, out ToReturn.csa);
+            if (succeededconnection == false)
+            {
+                throw new Exception("Connection to Azure blob storage using the provided connection string failed.");
+            }
+
+            //Make CBC
+            ToReturn.cbc = ToReturn.csa.CreateCloudBlobClient();
+            
+            return ToReturn;
+        }
+    
+        /// <summary>
+        /// Sets up all necessary blob containers (if they do not exist)
+        /// </summary>
+        public async Task InitializeAsync()
+        {   
+            await cbc.GetContainerReference("sessions").CreateIfNotExistsAsync();
+            await cbc.GetContainerReference("sessionsummaries").CreateIfNotExistsAsync();
+            await cbc.GetContainerReference("sessionanalyses").CreateIfNotExistsAsync();
+            await cbc.GetContainerReference("useraccounts").CreateIfNotExistsAsync();
+            await cbc.GetContainerReference("userphotos").CreateIfNotExistsAsync();
+            await cbc.GetContainerReference("wallpapers").CreateIfNotExistsAsync();
+        }
+    
+
+        #region "Listing data"    
+        public async Task<string[]> GetSessionNamesAsync()
+        {
+            string[] tr = await GetBlobNamesInContainerAsync("sessions");
+            return tr;
+        }
+    
+        public async Task<string[]> GetSessionSummaryNamesAsync()
+        {
+            string[] tr = await GetBlobNamesInContainerAsync("sessionsummaries");
+            return tr;
+        }
+    
+        public async Task<string[]> GetSessionAnalysisNamesAsync()
+        {
+            string[] tr = await GetBlobNamesInContainerAsync("sessionanalyses");
+            return tr;
+        }
+        #endregion
+
+        #region "Basic uploading"
+
+        public async Task UploadSessionAsync(List<byte[]> session_data)
+        {
+            //Get unique session
+            string file_name = "";
+            try
+            {
+                Packet p = new Packet();
+                p.LoadBytes(session_data[0]);
+                file_name = p.UniqueSessionId.ToString();
+            }
+            catch
+            {
+                throw new Exception("Fatal error while getting unique session ID.");
+            }
+            
+            
+            CloudBlobContainer cont = cbc.GetContainerReference("sessions");
+            await cont.CreateIfNotExistsAsync();
+
+            //Upload
+            CloudBlockBlob blb = cont.GetBlockBlobReference(file_name);
+            await blb.UploadTextAsync(JsonConvert.SerializeObject(session_data));
+        }
+
+        #endregion
+
+        // UTLITY FUNCTIONS BELOW
+        private async Task<string[]> GetBlobNamesInContainerAsync(string container_name)
+        {
+            List<string> ToReturn = new List<string>();
+
+            CloudBlobContainer cont = cbc.GetContainerReference(container_name);
+            await cont.CreateIfNotExistsAsync();
+
+            IEnumerable<IListBlobItem> blobs = cont.ListBlobs();
+            foreach (IListBlobItem bi in blobs)
+            {
+                CloudBlockBlob blb = (CloudBlockBlob)bi;
+                ToReturn.Add(blb.Name);
+            }
+
+            return ToReturn.ToArray();
+        }
+    }
+}
