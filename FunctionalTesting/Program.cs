32726700826c3ae2a8a932773f930d31c1401c43
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-07-23 20:40:48 -0700

    Did some testing

--- FunctionalTesting/Program.cs
+++ FunctionalTesting/Program.cs
@@ -15,33 +15,10 @@ namespace FunctionalTesting
             string constr = System.IO.File.ReadAllText("C:\\Users\\TaHan\\Downloads\\av_test_az_constr.txt");
             ApexVisualManager avm = ApexVisualManager.Create(constr);
 
-            string[] analysesssss = avm.LisSessionAnalysisNamesAsync().Result;
+            ApexVisualUserAccount acc = avm.DownloadUserAccountAsync("TimHanefwich").Result;
+            Console.WriteLine(acc.Email);
 
-            foreach (string s in analysesssss)
-            {
-                Console.WriteLine(s);
-            }
-            Console.ReadLine();
-            
-            string path = "C:\\Users\\TaHan\\Downloads\\Alfa Romeo Hungary 25 percent race.json";
-
-            string content = System.IO.File.ReadAllText(path);
-
-            List<byte[]> data = JsonConvert.DeserializeObject<List<byte[]>>(content);
-            Packet[] packets = CodemastersToolkit.BulkConvertByteArraysToPackets(data);
-
-            SessionAnalysis sa = new SessionAnalysis();
-            sa.Load(packets, packets[0].PlayerCarIndex);
-            while (sa.LoadComplete == false)
-            {
-                Console.WriteLine(sa.PercentLoadComplete.ToString("#0.0%"));
-                Task.Delay(1).Wait();
-            }
-
-            Console.WriteLine("Uploading...");
-            avm.UploadSessionAnalysisAsync(sa).Wait();
-
-            Console.WriteLine("Done!");
+            Console.WriteLine("Done");
 
         }
     }
