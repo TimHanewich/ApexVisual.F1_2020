3fe7d6ce6039b532c7ea7b270a03351ce6f8bffb
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-07-23 20:05:10 -0700

    Added ApexVisualUserAccount

---
+++ src/ApexVisualUserAccount.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ApexVisual.F1_2020
+{
+    public class ApexVisualUserAccount
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTimeOffset AccountCreatedAt { get; set; }
+        public List<string> OwnedSessionIds { get; set; }
+        public string PhotoBlobId { get; set; }
+
+        public ApexVisualUserAccount()
+        {
+            OwnedSessionIds = new List<string>();
+        }
+    }
+}
