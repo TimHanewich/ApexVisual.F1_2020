e31e41a552e998da18852151388cb992ad9b18af
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-10-01 21:40:24 -0400

    Added assets for message submissions to the owner

---
+++ src/MessageSubmission.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ApexVisual.F1_2020
+{
+    public class MessageSubmission
+    {
+        public string Email {get; set;}
+        public string Body {get; set;}
+        public MessageType MessageType {get; set;}
+        public DateTimeOffset CreatedAt {get; set;}
+    }
+}
---
+++ src/MessageType.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ApexVisual.F1_2020
+{
+    public enum MessageType
+    {
+        BugReport,
+        FeatureRecommendation
+    }
+}
