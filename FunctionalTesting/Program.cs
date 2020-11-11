776591d3ab8e9beb9551451f624c732de867defc
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-11-10 20:34:34 -0500

    Testing

--- FunctionalTesting/Program.cs
+++ FunctionalTesting/Program.cs
@@ -10,6 +10,7 @@ using System.Net;
 using ApexVisual.F1_2020.ActivityLogging;
 using ApexVisual.F1_2020.LiveSessionManagement;
 using System.IO;
+using ApexVisual.F1_2020.Analysis;
 
 namespace FunctionalTesting
 {
@@ -18,6 +19,21 @@ namespace FunctionalTesting
 
         static void Main(string[] args)
         {   
+            string content = System.IO.File.ReadAllText("C:\\Users\\tihanewi\\Downloads\\Silverstone 7 lap trash.json");
+            List<byte[]> bytes = JsonConvert.DeserializeObject<List<byte[]>>(content);
+            Packet[] packets = Packet.BulkLoadAllSessionData(bytes);
+            SessionAnalysis sa = new SessionAnalysis();
+            Console.WriteLine("Loading...");
+            sa.Load(packets, packets[0].PlayerCarIndex);
+            
+            foreach (CornerPerformanceAnalysis cpa in sa.Corners)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(cpa));
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+            
+
         }
 
         
