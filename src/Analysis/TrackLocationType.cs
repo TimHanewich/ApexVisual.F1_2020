f80424b2d6afa7fb0d003816954eef4be1dfbfcd
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-11-24 15:44:25 -0500

    TrackLocationType Enum

---
+++ src/Analysis/TrackLocationType.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApexVisual.F1_2020.Analysis
+{
+    public enum TrackLocationType
+    {
+        NotApplicable = 0,
+        Corner = 1,
+        SpeedTrap = 2
+    }
+}
