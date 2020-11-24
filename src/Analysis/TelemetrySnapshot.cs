47b97a06ac8229bc365884b4010938f4e0da7ad7
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-11-24 15:41:24 -0500

    Added new properties to TelemetrySnapshot

--- src/Analysis/TelemetrySnapshot.cs
+++ src/Analysis/TelemetrySnapshot.cs
@@ -15,6 +15,43 @@ namespace ApexVisual.F1_2020.Analysis
         public TelemetryPacket.CarTelemetryData Telemetry {get; set;}
         public CarStatusPacket.CarStatusData CarStatus {get; set;}
 
+        //The data
+        public float PositionX {get; set;}
+        public float PositionY {get; set;}
+        public float PositionZ {get; set;}
+        public float VelocityX {get; set;}
+        public float VelocityY {get; set;}
+        public float VelocityZ {get; set;}
+        public float gForceLateral {get; set;}
+        public float gForceLongitudinal {get; set;}
+        public float gForceVertical {get; set;}
+        public float Yaw {get; set;}
+        public float Pitch {get; set;}
+        public float Roll {get; set;}
+        public float CurrentLapTime {get; set;}
+        public byte CarPosition {get; set;}
+        public bool LapInvalid {get; set;}
+        public byte Penalties {get; set;}
+        public ushort SpeedKph {get; set;}
+        public float Throttle {get; set;}
+        public float Steer {get; set;}
+        public float Brake {get; set;}
+        public float Clutch {get; set;}
+        public sbyte Gear {get; set;}
+        public int EngineRpm {get; set;}
+        public bool DrsActive {get; set;}
+        public WheelDataArray BrakeTemperature {get; set;}
+        public WheelDataArray TyreSurfaceTemperature {get; set;}
+        public WheelDataArray TyreInnerTemperature {get; set;}
+        public int EngineTemperature {get; set;}
+        public FuelMix SelectedFuelMix {get; set;}
+        public float FuelLevel {get; set;}
+        public WheelDataArray TyreWearPercentage {get; set;}
+        public WheelDataArray TyreDamagePercent {get; set;}
+        public float FrontLeftWingDamage {get; set;}
+        public float FrontRightWingDamage {get; set;}
+        public float RearWingDamage {get; set;}
+        public float ErsStored {get; set;}
 
         public float DistanceToApex()
         {
