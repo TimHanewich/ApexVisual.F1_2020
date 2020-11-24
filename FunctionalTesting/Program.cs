12a69ca16f58798fc8716c47ddb03c0370751d86
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-11-24 15:18:02 -0500

    Cleared funcitonal testing

--- FunctionalTesting/Program.cs
+++ FunctionalTesting/Program.cs
@@ -16,27 +16,9 @@ namespace FunctionalTesting
 {
     class Program
     {
-
         static void Main(string[] args)
-        {   
-            string content = System.IO.File.ReadAllText("C:\\Users\\tihanewi\\Downloads\\Silverstone 7 lap trash.json");
-            List<byte[]> bytes = JsonConvert.DeserializeObject<List<byte[]>>(content);
-            Packet[] packets = Packet.BulkLoadAllSessionData(bytes);
-            SessionAnalysis sa = new SessionAnalysis();
-            Console.WriteLine("Loading...");
-            sa.Load(packets, packets[0].PlayerCarIndex);
-            
-            foreach (CornerPerformanceAnalysis cpa in sa.Corners)
-            {
-                Console.WriteLine(JsonConvert.SerializeObject(cpa));
-                Console.WriteLine();
-                Console.ReadLine();
-            }
-            
+        {              
 
         }
-
-        
-    
     }
 }
