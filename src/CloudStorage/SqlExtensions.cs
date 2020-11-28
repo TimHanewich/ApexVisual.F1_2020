82e8cafd867a9bac5e53d4bf367f92fc07995c12
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-11-28 09:41:34 -0500

    Delete lap method

--- src/CloudStorage/SqlExtensions.cs
+++ src/CloudStorage/SqlExtensions.cs
@@ -999,6 +999,14 @@ namespace ApexVisual.F1_2020.CloudStorage
             await sqlcmd.ExecuteNonQueryAsync();
         }
 
+        public async static Task DeleteLapAsync(this ApexVisualManager avm, Guid id)
+        {
+            string cmd = "delete from Lap where Id='" + id.ToString() + "'";
+            SqlConnection sqlcon = GetSqlConnection(avm);
+            SqlCommand sqlcmd = new SqlCommand(cmd, sqlcon);
+            await sqlcmd.ExecuteNonQueryAsync();
+        }
+
         #endregion
     }
 }
