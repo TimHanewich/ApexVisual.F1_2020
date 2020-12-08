41798d5e15817792ad171c5cb79e000294bcde22
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-12-08 16:50:06 -0500

    Created hybrid storage extensions class

---
+++ src/CloudStorage/HybridStorageExtensions.cs
@@ -0,0 +1,11 @@
+using System;
+using ApexVisual.F1_2020;
+using ApexVisual.F1_2020.CloudStorage;
+
+namespace ApexVisual.F1_2020.CloudStorage
+{
+    public static class HybridStorageExtensions
+    {
+        
+    }
+}
