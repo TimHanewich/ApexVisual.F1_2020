8813f20ff4c4aec9d469706568a224edc8d0834e
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-12-13 14:38:32 -0500

    Comments on session retrieval depth

--- src/CloudStorage/SessionRetrievalDepth.cs
+++ src/CloudStorage/SessionRetrievalDepth.cs
@@ -4,8 +4,8 @@ namespace ApexVisual.F1_2020.CloudStorage
 {
     public enum SessionRetrievalDepth
     {
-        Summary = 0,
-        Lap = 1,
-        Full = 2
+        Summary = 0, //Just the details about the session (track, team, driver, created at, etc)
+        Lap = 1, //Everything above, and then details about each lap (Sector times, lap invalid, etc.)
+        Full = 2 //ALL DATA
     }
 }
