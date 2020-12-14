d00f9048ea20b6f412616b860cc065d79723f8b8
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-12-14 17:39:59 -0500

    More detailed activity logging

--- src/ActivityLogging/ActivityType.cs
+++ src/ActivityLogging/ActivityType.cs
@@ -15,17 +15,22 @@ namespace ApexVisual.F1_2020.ActivityLogging
         RegisterClickedOnHomePage = 9,
         LoginAttemptedOnHomePage = 10,
 
+        //Home page
+        HomePageOpened = 20,
+
         //tutorial
         TutorialClosed = 2, // User pressed 'Let's race!' at the bottom of the tutorial page
         TutorialOpened = 3, //User pressed Tutorial on the home page to open the tutorial
 
         //Livd display
-        LiveDisplayOpened = 4, //User pressed on live display on the home page
+        LiveDisplayClicked = 4, //User pressed on live display on the home page
         LiveDisplayStartListeningClicked = 11, //User clicked on 'start listening'
         LiveDisplaySaveSessionClicked = 12, //User clicked save session
-        LiveDisplayOpenSessionClicked = 13, //User clicked open session
+        LiveDisplayOpenSessionClicked = 13, //User clicked open session (now deprecated)
         LiveDisplaySaveToCloudClicked = 14, //User clicked on save to cloud
         LiveDisplaySaveToLocalClicked = 15, //User clicked on save to local
+        LiveDisplaySaveToCloudFailed = 21, //The upload to cloud failed
+        LiveDisplaySaveToCloudSucceeded = 22, //The upload to cloud succeeded (it is now in the cloud)
 
         //Analysis
         AnalysisOpened = 5, //User pressed on analysis on the home page
