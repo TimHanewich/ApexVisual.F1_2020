07657f0e30ab86361ed0175d21a3747191368c67
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-12-15 21:21:06 -0500

    Made selected tyre compound a public property

--- src/LiveSessionManagement/LiveDriverSessionData.cs
+++ src/LiveSessionManagement/LiveDriverSessionData.cs
@@ -28,7 +28,7 @@ namespace ApexVisual.F1_2020.LiveSessionManagement
         public LapPacket.DriverStatus Qualifying_DriverStatus {get; set;}
 
         //Other agnostic data
-        TyreCompound SelectedTyreCompound {get; set;}
+        public TyreCompound SelectedTyreCompound {get; set;}
         public int TyreAgeLaps {get; set;}
 
         #region "Misc Variables for internal tracking"
